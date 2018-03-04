e1562ac7facd2096a16533d6495b5bbe2dae2930
0xSteve <steven.porretta@gmail.com>
2018-03-04 14:28:38 -0500

    added in the helper file from assignment 2

---
+++ generator.py
@@ -0,0 +1,12 @@
+'''This is the main script for the assignment.'''
+
+import numpy as np
+from numpy import linalg as la
+from helper import *
+
+# I made some mistakes with my assignment 2, so I've updated my
+# diagonalizing code from some guaranteed correct code from Omar.
+# The parts that are from assignment 2 have been corrected thanks to Omar's
+# help.
+
+np.array(multi_normal_vector(sizeof, 3))
---
+++ helper.py
@@ -0,0 +1,49 @@
+'''Some helper files for this work.'''
+
+from random import uniform as rand
+import numpy as np
+from numpy import linalg as LA
+import math
+
+
+def normal_vector(size):
+    '''Generate a Normally distributed vector from a uniform pseudo-random
+       number generator.'''
+    vec = []
+    for i in range(size):
+        temp = 0
+        a = 0
+        for k in range(12):
+            a = rand(0, 1)
+            temp += a
+        temp = temp - 6
+        vec.append(temp)
+    return vec
+
+
+def multi_normal_vector(size, dimensions=2):
+    '''Generate a Normally distributed vector from a uniform pseudo-random
+       number generator.'''
+
+    vec = []
+    for i in range(size):
+        x = []
+        for j in range(dimensions):
+            temp = 0
+            a = 0
+            for k in range(12):
+                a = rand(0, 1)
+                temp += a
+            temp = temp - 6
+            x.append(temp)
+        vec.append(x)
+    return vec
+
+
+def inv_sqrt(A):
+    '''Perform the element wise inverse square root. Assumes m x n matrix.s'''
+    for i in range(len(A)):
+        for j in range(len(A[1])):
+            A[i][j] = math.pow(A[i][j], -0.5)
+
+    return A
