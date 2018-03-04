b739267b0d9aabb41f266096edc5d1694bbfe596
0xSteve <steven.porretta@gmail.com>
2018-03-04 18:27:20 -0500

    Created a better Z vector function.

--- helper.py
+++ helper.py
@@ -6,23 +6,9 @@ from numpy import linalg as LA
 import math
 
 
-def normal_vector(size):
-    '''Generate a Normally distributed vector from a uniform pseudo-random
-       number generator.'''
-    vec = []
-    for i in range(size):
-        temp = 0
-        a = 0
-        for k in range(12):
-            a = rand(0, 1)
-            temp += a
-        temp = temp - 6
-        vec.append(temp)
-    return vec
-
-
 def normal_point(dimensions=3):
-    '''Generate a point from a standard normal distribution.'''
+    '''Generate a Normally distributed point from a multivariate Normal
+       distribution of a specified dimension, by default 3.'''
     x = []
     for i in range(dimensions):
         z = 0
@@ -35,27 +21,26 @@ def normal_point(dimensions=3):
     return x
 
 
-def multi_normal_vector(size, dimensions=2):
-    '''Generate a Normally distributed vector from a uniform pseudo-random
-       number generator.'''
+def make_Z(size=200, dimensions=3):
+    '''Create an array of Normally distributed points.'''
+    Z = normal_point(dimensions)  # Will become a non-empty numpy array.
 
-    vec = []
-    for i in range(size):
-        x = []
-        for j in range(dimensions):
-            temp = 0
-            a = 0
-            for k in range(12):
-                a = rand(0, 1)
-                temp += a
-            temp = temp - 6
-            x.append(temp)
-        vec.append(x)
-    return vec
+    for i in range(size - 1):
+        point = normal_point(dimensions)
+        # add the new point on axis 1 to create a new column.
+        Z = np.append(Z, point, axis=1)
+
+    return Z
+
+
+def make_X(Z, lambda_x, mean, eigvec, size=200, dimensions=3):
+    '''Given a standard normal vector Z, a mean, and eigen values and vectors of
+       variance, generate a translated normal vector X.'''
+    x = Z[0]
 
 
 def inv_sqrt(A):
-    '''Perform the element wise inverse square root. Assumes m x n matrix.s'''
+    '''Perform the element wise inverse square root. Assumes m x n matrix.'''
     for i in range(len(A)):
         for j in range(len(A[1])):
             A[i][j] = math.pow(A[i][j], -0.5)
