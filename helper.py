6267df2fa803f557cc3e51ec582a7e30a3827406
0xSteve <steven.porretta@gmail.com>
2018-03-04 18:33:56 -0500

    Correct the mean for the make_X vector.

--- helper.py
+++ helper.py
@@ -36,10 +36,10 @@ def make_Z(size=200, dimensions=3):
 def make_X(Z, lambda_x, mean, eigvec, size=200, dimensions=3):
     '''Given a standard normal vector Z, a mean, and eigen values and vectors of
        variance, generate a translated normal vector X.'''
-    x = eigvec @ np.power(lambda_x, 0.5) @ Z[0] @ mean
+    x = eigvec @ np.power(lambda_x, 0.5) @ Z[0] + mean
 
     for i in range(size - 1):
-    point = eigvec @ np.power(lambda_x, 0.5) @ Z[0] @ mean
+    point = eigvec @ np.power(lambda_x, 0.5) @ Z[0] + mean
 
 
 def inv_sqrt(A):
